fc8cb88a460340dca82c61eaf23df06fe249df48
Aditya Saxena <adityasmksaxena@gmail.com>
2018-05-25 15:52:19 +0530

    scroolToBottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 const socket = io();
 
+const scrollToBottom = () => {
+  // Selectors
+  const messages = jQuery('#messages');
+  const newMessage = messages.children('li:last-child');
+  // Heights
+  const clientHeight = messages.prop('clientHeight');
+  const scrollTop = messages.prop('scrollTop');
+  const scrollHeight = messages.prop('scrollHeight');
+  const newMessageHeight = newMessage.innerHeight();
+  const lastMessageHeight = newMessage.prev().innerHeight();
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+    console.log('should scroll');
+  }
+};
+
+
 socket.on('connect', () => {
   console.log('Connected to Server');
 });
@@ -10,6 +27,7 @@ socket.on('newMessage', (message) => {
   const template = jQuery('#message-template').html();
   const html = Mustache.render(template, { from, text, createdAt: formatedTime });
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', (message) => {
@@ -19,6 +37,7 @@ socket.on('newLocationMessage', (message) => {
   const template = jQuery('#location-message-template').html();
   const html = Mustache.render(template, { from, url, createdAt: formatedTime });
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('disconnect', () => {
